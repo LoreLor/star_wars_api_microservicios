d605c29113faf22b62d0d680b6a84d85915adf23
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-11 17:22:34 -0300

    port_db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const server= require('./src/server')
-const PORT = 8004
 
 
-server.listen(PORT, () => {
-    console.log(`DATABASE listening on port: ${PORT}`);
+server.listen(8004, () => {
+    console.log(`DATABASE listening on port: 8004`);
 })
